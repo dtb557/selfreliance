3c44d4ff483061d92529ae7a6ede0af96d1fd72e
Derek T. Burk <burkx031@umn.edu>
2017-09-07 16:34:55 -0500

    sqrt_famsize should be calculated from full data, not imputation subset!

--- main/4_fix_pre_imputation_problems/1_fix_sqrt_famsize.R
+++ main/4_fix_pre_imputation_problems/1_fix_sqrt_famsize.R
@@ -3,20 +3,19 @@ library(magrittr)
 library(dplyr)
 
 load("main/1_clean_data/cleaned_data_step_5.Rdata")
-subfamid_matcher <- d %>%
-    select(year, serial, pernum, subfamid)
+fixed_sqrt_famsize <- d %>%
+    select(-sqrt_famsize) %>%
+    group_by(year, serial, subfamid) %>%
+    mutate(sqrt_famsize = n()) %>%
+    ungroup() %>%
+    select(year, serial, pernum, sqrt_famsize)
 rm(d)
 
 for(yr in seq(1970, 2010, by=10)) {
     load(sprintf("main/3_multiply_impute/3_imp_%d_10_extreme_values_transposed.Rdata", yr))
     imp$data <- imp$data %>%
-        rename(sqrt_hh_size = sqrt_famsize)
+        rename(sqrt_hh_size = sqrt_famsize) %>%
+        left_join(fixed_sqrt_famsize, by=c("year", "serial", "pernum"))
     names(imp$imp)[names(imp$imp) == "sqrt_famsize"] <- "sqrt_hh_size"
-    imp$data <- imp$data %>%
-        left_join(subfamid_matcher, by=c("year", "serial", "pernum")) %>%
-        group_by(year, serial, subfamid) %>%
-        mutate(sqrt_famsize=n()) %>%
-        ungroup() %>%
-        select(-subfamid)
     save(imp, file=sprintf("main/4_fix_pre_imputation_problems/1_imp_%d_10_fixed.Rdata", yr))
 }
