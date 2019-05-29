efeafb950e0219dc575d3a2f043c5d02c46deede
Derek Burk <burkx031@umn.edu>
2019-05-29 06:43:22 -0500

    Skip first line of output files

--- functions/check_taxsim_input_output.R
+++ functions/check_taxsim_input_output.R
@@ -5,7 +5,8 @@ check_taxsim_input_output <- function(input_dir, output_dir) {
     for(f in in_files) {
         cat(f, "\n")
         in_f <- read.table(file.path(input_dir, f), quote="", skip=1)
-        out_f <- read.table(file.path(output_dir, paste0(f, ".taxsim")), quote="") 
+        out_f <- read.table(file.path(output_dir, paste0(f, ".taxsim")), 
+                            quote = "", skip = 1) 
         if(nrow(in_f)==nrow(out_f)) next
         else stop(paste0("Length of input and output do not match for ", f))
     }
